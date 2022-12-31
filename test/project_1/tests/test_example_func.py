2139dbf76c3cdee3d364bf5aefa912919e5441e1
Freddie Sherratt <fsherratt@gmail.com>
2022-12-31 12:46:26 +0000

    Add unit test

--- test/project_1/tests/test_example_func.py
+++ test/project_1/tests/test_example_func.py
@@ -10,3 +10,7 @@ class TestClass(unittest.TestCase):
     def test_add(self):
         """Test Add function"""
         self.assertEqual(add(2, 4), 6)
+
+    def test_add_2(self):
+        """Test Add function"""
+        self.assertEqual(add(4, -2), 2)
